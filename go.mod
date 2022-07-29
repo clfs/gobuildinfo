ce307698a752f0c2ddddacf5ef45bf80b06d500c
Calvin Figuereo-Supraner <mail@calvin.page>
2022-07-29 10:44:26 -0700

    Add tool

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/clfs/gobuildinfo
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"debug/buildinfo"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("usage: gobuildinfo FILE")
+	}
+
+	info, err := buildinfo.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("failed to read build info: %v", err)
+	}
+
+	data, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		log.Fatalf("failed to convert build info into JSON: %v", err)
+	}
+
+	fmt.Printf("%s\n", data)
+}
